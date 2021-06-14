bfd5ab6492889ad6ad6705a8f615982f4b62328c
Yan Marcos <yanmsn97@gmail.com>
2021-06-14 03:41:07 -0300

    adding moviment to the particles

--- sketch.js
+++ sketch.js
@@ -60,10 +60,14 @@ function mouseClicked() {
 class Particle {
   constructor() {
     this.pos = p5.Vector.random2D().mult(250)
+    this.vel = createVector(0, 0)
+    this.acc = this.pos.copy().mult(random(0.0001, 0.00001))
+    this.w = random(3, 5)
   }
+
   show() {
     noStroke()
     fill(255)
-    ellipse(this.pos.x, this.pos.y, 4)
+    ellipse(this.pos.x, this.pos.y, this.w)
   }
 }
