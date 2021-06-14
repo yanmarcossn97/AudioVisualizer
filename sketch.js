b06f1f7c91c6738c36c8ed81deafc3f4321094c2
Yan Marcos <yanmsn97@gmail.com>
2021-06-14 02:36:14 -0300

    adding shapes to waveform

--- sketch.js
+++ sketch.js
@@ -20,14 +20,17 @@ function draw() {
 
   var wave = fft.waveform()
 
+  beginShape()
   for(var i = 0; i < width; i++) {
     var index = floor(map(i, 0, width, 0, wave.length))
 
     var x = i
     var y = wave[index] * 300 + height / 2
 
-    point(x, y)
+    // point(x, y)
+    vertex(x, y)
   }
+  endShape()
 }
 
 function mouseClicked() {
