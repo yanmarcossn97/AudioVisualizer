75c8ed3ea35d06d88510d501f81cfb38755e1ee8
Yan Marcos <yanmsn97@gmail.com>
2021-06-14 12:21:16 -0300

    capturing frames from canvas with CCapture

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
 // browser-sync --start --server --files "*"
 // console.clear()
+// ffmpeg -r 30 -f image2 -s 540x540 -i "%07d.png" -vcodec libx264 -crf 17 -pix_fmt yuv420p output.mp4
 
 var song
 var fft
 var particles = []
 var img
+
+// the frame rate
+var fps = 60;
+
+// the canvas capturer instance
+var capturer = new CCapture({ format: 'png', framerate: fps });
 
 function preload() {
   song = loadSound('destiny.mp3')
@@ -19,9 +26,43 @@ function setup() {
   fft = new p5.FFT(0.3)
 
   img.filter(BLUR, 12)
+
+  // this is optional, but lets us see how the animation will look in browser.
+  frameRate(fps);
 }
 
+// needed to subtract initial millis before first draw to 
+var startMillis; 
+
 function draw() {
+  if (frameCount === 1) {
+    // start the recording on the first frame
+    // this avoids the code freeze which occurs if capturer.start is called
+    // in the setup, since v0.9 of p5.js
+    capturer.start();
+  }
+
+  if (startMillis == null) {
+    startMillis = millis();
+  }
+
+  // duration in milliseconds
+  var duration = 3000;
+
+  // compute how far we are through the animation as a value between 0 and 1.
+  var elapsed = millis() - startMillis;
+  var t = map(elapsed, 0, duration, 0, 1);
+
+  // if we have passed t=1 then end the animation.
+  if (t > 1) {
+    noLoop();
+    console.log('finished recording.');
+    capturer.stop();
+    capturer.save();
+    return;
+  }
+
+  // actually draw
   background(150);
   translate(width / 2, height / 2)
 
@@ -41,7 +82,7 @@ function draw() {
   noStroke()
   rect(0, 0, width, height)
 
-  stroke(255)
+  stroke(250)
   strokeWeight(3)
   noFill()
 
@@ -74,6 +115,11 @@ function draw() {
       particles.splice(i, 1)
     }
   }
+  // end drawing code
+
+  // handle saving the frame
+  console.log('capturing frame');
+  capturer.capture(document.getElementById('defaultCanvas0'));
 }
 
 function mouseClicked() {
@@ -92,7 +138,7 @@ class Particle {
     this.vel = createVector(0, 0)
     this.acc = this.pos.copy().mult(random(0.0001, 0.00001))
     this.w = random(3, 5)
-    this.color = [random(200, 255), random(200, 255), random(200, 255)]
+    this.color = [random(200, 255), random(150, 200), random(150, 200)]
   }
 
   update(cond) {
